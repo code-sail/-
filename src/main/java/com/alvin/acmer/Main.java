a27f07f95b6ad85adb381151c41852bca4733d7f
code-sail <codesail1911@163.com>
2024-11-21 23:59:00 +0800

    2024.11.21

--- src/main/java/com/alvin/acmer/Main.java
+++ src/main/java/com/alvin/acmer/Main.java
@@ -219,27 +219,75 @@ import java.util.Scanner;
 /**
  * 差分
  */
+//public class Main{
+//    public static int N = 100010;
+//    public static int [] a = new int[N], b = new int[N];
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt(), m = sc.nextInt();
+//        for(int i = 1; i <= n; i ++) {
+//            int k = sc.nextInt();
+//            b[i] += k;
+//            b[i + 1] -= k;
+//        }
+//        while(m -- > 0) {
+//            int l = sc.nextInt(), r = sc.nextInt(), c = sc.nextInt();
+//            b[l] += c;
+//            b[ r + 1] -= c;
+//        }
+//
+//        for(int i = 1; i <= n; i ++)    b[i] += b[i - 1];
+//        for(int i = 1; i <= n; i ++) System.out.println(b[i]);
+//    }
+//
+//}
+
+/**
+ * 差分矩阵（二维差分）
+ */
+import java.util.Scanner;
+
 public class Main{
-    public static int N = 100010;
-    public static int [] a = new int[N], b = new int[N];
-
-    public static void main(String[] args) {
+    public static int N = 1010;
+    public static int [][] b = new int[N][N];
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt();
-        for(int i = 1; i <= n; i ++) {
-            int k = sc.nextInt();
-            b[i] += k;
-            b[i + 1] -= k;
-        }
-        while(m -- > 0) {
-            int l = sc.nextInt(), r = sc.nextInt(), c = sc.nextInt();
-            b[l] += c;
-            b[ r + 1] -= c;
+        int n = sc.nextInt(), m = sc.nextInt(), q = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                insert(i, j, i, j, sc.nextInt());
+            }
         }
 
-        for(int i = 1; i <= n; i ++)    b[i] += b[i - 1];
-        for(int i = 1; i <= n; i ++) System.out.println(b[i]);
+        while(q -- > 0){
+            int x1 = sc.nextInt(), y1 = sc.nextInt(), x2 = sc.nextInt(), y2 = sc.nextInt();
+            int c = sc.nextInt();
+            insert(x1, y1, x2, y2, c);
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                b[i][j] = b[i][j] + b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1];
+            }
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                System.out.printf("%d ", b[i][j]);
+            }
+            System.out.println();
+        }
+
+
     }
 
+    public static void insert(int x1, int y1, int x2, int y2, int c){
+        b[x1][y1] += c;
+        b[x2 + 1][y2] -= c;
+        b[x2][y2 + 1] -= c;
+        b[x2 + 1][y2 + 1] += c;
+    }
 }
 
